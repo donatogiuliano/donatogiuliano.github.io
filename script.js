087a4c4533bff57cc2ba21b434047677b6300f65
Donato <giulianodonato13@gmail.com>
2022-07-06 11:56:42 +0200

    d

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector('#berechnen').addEventListener('click', berechne_BMI);
+
+function berechne_BMI() {
+    var groesse = parseInt(document.getElementById("groesse").value),
+        masse = parseInt(document.getElementById("masse").value),
+        bmi = Math.round(100000 * masse / (groesse * groesse)) / 10;
+
+    document.querySelector("output").textContent = bmi;
+}
